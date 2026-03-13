fdfe48b31d014deba9197198311eea213a860ec2
Jagrit <jagrit4code@gmail.com>
2026-03-14 01:03:33 +0530

    expense tracker in progress

---
+++ exp.py
@@ -0,0 +1,10 @@
+def add_with_return(a,b):
+    return a+b 
+def add_with_print(a, b):
+    print(a+ b)
+result = add_with_return(3,5)
+print(result * 2)
+
+result_print = add_with_print(3,5)
+print(result_print * 2 )
+
